1d3791b2b22ec98702846fafae7ecc3d941bc081
aleksandra-gruszczyk <98498980+aleksandra-gruszczyk@users.noreply.github.com>
2022-12-01 09:41:00 +1300

    chore: Add card and icons (#18)

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,23 +1,50 @@
+import { Badge, Button, Card, Group, Image, Text } from '@mantine/core'
 import React, { useEffect, useState } from 'react'
+import MdRotateRight from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+
 import { fetchJobs } from '../actions/jobs'
 
-useEffect(() => {
-  dispatch(fetchJobs())
-}, [])
+export default function JobCard() {
+  const jobs = useSelector((state) => state.jobs)
 
-export default function JobList(){
-  return(
-    <section>
-        <ul>
-          {jobs.map((job, i) => (
-            <li key={i}>
-              
-                <h2>{job.company_name}</h2> <h3>{job.role}</h3> {job.location} {job.requirements} {job.byline}
-                {job.logo}
-                </li>
-          ))}
-        </ul>
-      </section>
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchJobs())
+  }, [])
+
+  return (
+    <ul>
+      {jobs.map((job) => (
+        <li key={job.id}>
+          <Card shadow='sm' p='lg' radius='md' withBorder>
+            <Card.Section>
+              <Image src={job.logo} />
+            </Card.Section>
+            <Group position='apart' mt='md' mb='xs'>
+              <Text weight={500}>{job.company_name}</Text>
+              <Badge color='orange' variant='light'>
+                {job.location}
+              </Badge>
+            </Group>
+            <Text size='sm'>{job.role}</Text>
+            <Text size='sm' color='dimmed'>
+              {job.byline}
+            </Text>
+            <Button
+              variant='light'
+              color='blue'
+              mt='md'
+              radius='md'
+              // onClick={handleClick}
+            >
+              <MdRotateRight />
+            </Button>
+          </Card>
+        </li>
+      ))}
+    </ul>
   )
 }
