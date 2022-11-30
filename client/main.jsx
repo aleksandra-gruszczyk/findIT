9b5c51b391ce4b1f9d3adf9e5c402a8dfdab6a2c
Kyle <112350799+kyle-skaxx@users.noreply.github.com>
2022-11-30 17:25:28 +1300

    chore: Add Mantine (#2)

--- client/main.jsx
+++ client/main.jsx
@@ -1,14 +1,18 @@
+import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+
 import App from './App'
 import GlobalStyles from './GlobalStyles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Router>
-      <App />
-    </Router>
+    <MantineProvider>
+      <GlobalStyles />
+      <Router>
+        <App />
+      </Router>
+    </MantineProvider>
   </React.StrictMode>
 )
