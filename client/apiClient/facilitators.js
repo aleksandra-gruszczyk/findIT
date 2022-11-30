6c844d1d640febec4a55c9291fd6bb957a0c35c5
Brennan Kinney <5098581+polarathene@users.noreply.github.com>
2022-11-30 14:36:46 +1300

    chore: Strip out lots of stuff

--- client/apiClient/facilitators.js
+++
@@ -1,9 +0,0 @@
-import request from 'superagent'
-
-export function fetchFacilitators() {
-  return request.get('/api/facilitators').then((res) => res.body.facilitators)
-}
-
-export function deleteFacilitator(id) {
-  return request.delete(`/api/facilitators/${id}`).then((res) => res.body)
-}
--- client/main.jsx
+++ client/main.jsx
@@ -1,21 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import { Auth0ProviderWithRedirectCallback } from './utils/auth'
-
-const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Auth0ProviderWithRedirectCallback>
-      <QueryClientProvider client={client}>
-        <App />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </Auth0ProviderWithRedirectCallback>
+    <App />
   </BrowserRouter>
 )
