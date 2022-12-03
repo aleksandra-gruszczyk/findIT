c526ec0352486d3376456bf56563553b11d54949
Brennan Kinney <5098581+polarathene@users.noreply.github.com>
2022-12-03 16:19:44 +1300

    chore: Remove JobDetails page and `<TestSlide />` (#56)

--- client/App.jsx
+++ client/App.jsx
@@ -8,7 +8,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Pages.Home />} />
         <Route path='/saved/:id' element={<Pages.SavedJobs />} />
-        <Route path='/job/:id' element={<Pages.JobDetails />} />
         <Route path='/jobs/swipe' element={<Pages.Deck />} />
       </Routes>
     </main>
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,10 +1,8 @@
 import Deck from './Deck'
 import Home from './Home'
-import JobDetails from './JobDetails'
 import SavedJobs from './SavedJobs'
 export default {
   Home,
-  JobDetails,
   SavedJobs,
   Deck,
 }
