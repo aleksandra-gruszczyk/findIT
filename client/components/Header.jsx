0d2e8e212c61567655a42fda126d3beabd9abb93
Brennan Kinney <5098581+polarathene@users.noreply.github.com>
2022-12-05 22:20:48 +1300

    chore: Better manage and document theming examples

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Group, Header } from '@mantine/core'
+import { Box, Group, Header } from '@mantine/core'
 import { Link } from 'react-router-dom'
 
 import Logo from './Logo'
@@ -10,17 +10,19 @@ export default function NavHeader({ children }) {
         height={80}
         px='md'
         // Without theme, manual inline styles:
+        // Has priority over sx and other style overrides:
         // style={{
         //   // backgroundColor: '#2a2b3e',
         //   // backgroundColor: 'rgb(59 65 91)',
         //   backgroundColor: '#f6f6f6',
         // }}
-        // Theme override on mantine components:
+        // Theme override on mantine components,
+        // use a color[shade] based on the current color scheme:
         sx={(theme) => ({
           backgroundColor:
             theme.colorScheme === 'dark'
               ? theme.colors.dark[8]
-              : theme.colors.light[1],
+              : theme.colors.customLight[0],
         })}
       >
         <Group position='apart' sx={{ height: '100%' }}>
