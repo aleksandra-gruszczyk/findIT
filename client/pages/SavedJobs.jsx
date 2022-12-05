d8d18256452579b866bbd50e5effbeb64fddcb94
Brennan Kinney <5098581+polarathene@users.noreply.github.com>
2022-12-05 16:43:56 +1300

    chore: Change `jobFavourites.favourites` to store Job IDs instead of objects

--- client/pages/SavedJobs.jsx
+++ client/pages/SavedJobs.jsx
@@ -1,16 +1,18 @@
 import { JobCard } from '@components/Card'
 import { css } from '@emotion/react'
 import useStoreJobFavourites from '@store/jobFavourites'
+import useStoreJobs from '@store/jobs'
 
 export default function SavedJobs() {
+  const jobs = useStoreJobs((state) => state.jobs)
   const favourites = useStoreJobFavourites((state) => state.favourites)
 
   // Tack on the index because who knows why
   // (but avoids a rendering bug with removals if same job id)
   // Yet if all have the key 0, the problem doesn't happen :/
-  const savedJobs = favourites.map((favouriteJob, i) => (
-    <li key={favouriteJob.id + '-' + i}>
-      <JobCard job={favouriteJob} showRemove={true} />
+  const savedJobs = favourites.map((jobId, i) => (
+    <li key={jobId + '-' + i}>
+      <JobCard job={jobs[jobId]} showRemove={true} />
     </li>
   ))
 
